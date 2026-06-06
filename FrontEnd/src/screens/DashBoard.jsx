d01a3af063e1878ab54794981b7ce42762a14d80
sonubhardwajj19 <sonubhardwajj19@gmail.com>
2026-06-06 23:29:22 +0530

    updated

--- FrontEnd/src/screens/DashBoard.jsx
+++ FrontEnd/src/screens/DashBoard.jsx
@@ -6,6 +6,7 @@ import { motion } from "framer-motion";
 import Calendar from "../Components/Calendar";
 
 import "react-calendar/dist/Calendar.css";
+import { Footer } from "../Components/Footer";
 
 
 export function DashBoard () {
@@ -328,37 +329,11 @@ export function DashBoard () {
                         }
 
                         </div>
-            </div>
-            
-      </div>
-          
-
-
-
-            
-
-             <div id="footer" className="h-150 w-full bg-black border-y-2 border-stone-900 flex flex-col ">
-                     <div className="flex ml-50 mt-10 gap-60">
-                  <div>
-                     <h2 className="text-yellow-900 text-[40px] font-semibold">Attendle</h2>
-                     <div> <p className="font-semibold text-gray-400">© 2026 Attendle. All rights reserved.</p></div>
-                  </div>
-                  <div className="mt-1">
-                     <div> <a href="policies" className="font-semibold text-gray-400 hover:text-white">Privacy Policy</a></div>
-                     <div> <a href="terms&conditions" className="font-semibold text-gray-400 hover:text-white">Terms & Conditions</a></div>  
-                  </div>
-                  <div className=" mt-1"> 
-                     <p className="font-semibold text-gray-400 hover:text-white">Drop your queries at</p>
-                     <p className="font-semibold text-gray-400 hover:text-white">zsb1926@gmail.com</p>
-                  </div>
                </div>
-
-
-
-               <div className="mt-auto">
-                  <h1 className=" bg-gradient-to-b from-white to-black bg-clip-text text-transparent text-[300px] font-bold ">ATTENDLE</h1>
-                 </div>
-            </div>
+            
+         </div>
+          
+      <Footer/>
     
      </div>
     </>
--- FrontEnd/src/screens/Login.jsx
+++ FrontEnd/src/screens/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { TopBar } from "../Components/Topbar";
+import { Footer } from "../Components/Footer";
 
 export function Login () {
 
@@ -53,26 +54,7 @@ export function Login () {
 
 
 
-            <div id="footer" className="h-150 w-full bg-black border-y-2 border-stone-900 flex flex-col mt-60">
-                <div className="flex ml-50 mt-10 gap-60">
-                <div>
-                <h2 className="text-yellow-900 text-[40px] font-semibold">Attendle</h2>
-                <div> <p className="font-semibold text-gray-400">© 2026 Attendle. All rights reserved.</p></div>
-                </div>
-                <div className="mt-1">
-                <div> <a href="policies" className="font-semibold text-gray-400 hover:text-white">Privacy Policy</a></div>
-                <div> <a href="terms&conditions" className="font-semibold text-gray-400 hover:text-white">Terms & Conditions</a></div>  
-                </div>
-                <div className=" mt-1"> 
-                <p className="font-semibold text-gray-400 hover:text-white">Drop your queries at</p>
-                <p className="font-semibold text-gray-400 hover:text-white">zsb1926@gmail.com</p>
-                </div>
-            </div>
-
-            <div className="mt-auto">
-                <h1 className=" bg-gradient-to-b from-white to-black bg-clip-text text-transparent text-[300px] font-bold ">ATTENDLE</h1>
-            </div>
-            </div>
+         <Footer/>
 
 
        </div>
